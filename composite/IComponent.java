a55cb66a470ceaf767aba1642dec2a116edf2565
Tanzim Mokammel <tanzim.mokammel@moonfare.com>
2020-03-27 16:14:09 +0100

    composite exercise

---
+++ composite/IComponent.java
@@ -0,0 +1,10 @@
+public interface IComponent {
+
+    // Your code goes here!
+    void play();
+
+    void setPlaybackSpeed(float speed);
+
+    String getName();
+
+}
---
+++ composite/Playlist.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class Playlist implements IComponent {
+
+    public String playlistName;
+    public ArrayList<IComponent> playlist = new ArrayList<IComponent>();
+
+    public Playlist(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    // Your code goes here!
+    public void play() {
+        // play
+    }
+
+    public void setPlaybackSpeed(float speed) {
+        // set speed
+        for (IComponent song : playlist) {
+            song.setPlaybackSpeed(speed);
+        }
+    }
+
+    public String getName() {
+        return playlistName;
+    }
+
+    public void add(IComponent song) {
+        playlist.add(song);
+    }
+
+    public void remove(IComponent song) {
+        playlist.remove(song);
+    }
+
+}
